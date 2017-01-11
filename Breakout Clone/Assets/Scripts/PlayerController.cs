d464d4f4a7af283f022221019da2735f8be75898
Dominic Brown <domingomonk@gmail.com>
2017-01-11 00:27:46 -0500

    init commit. Made scene and set up scene; added player movement

---
+++ Breakout Clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed = 1;
+	public Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		rb.velocity = new Vector3(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime,0);
+	}
+
+}
